5822baf2c020ddcee4572223f04c76d1403a478b
call123456789 <2400017705@stu.pku.edu.cn>
2025-06-21 16:35:38 +0800

    Delete install.py

--- install.py
+++
@@ -1,5 +0,0 @@
-def pip_install(module):
-    import subprocess
-    import sys
-    install_cmd = [sys.executable, "-m", "pip", "install", module]
-    subprocess.run(install_cmd,capture_output=True,text=True)
